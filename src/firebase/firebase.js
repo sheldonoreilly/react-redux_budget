8d2551d98978383412202aff555153566c2a4823
Sheldon O'Reilly <sheldon.oreilly@gmail.com>
2018-08-27 12:02:58 -0230

    added environment viarables for dev and testing

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,11 +1,11 @@
 import * as firebase from "firebase";
 var config = {
-	apiKey: "AIzaSyDlbB74UobOBTrXrasewy8h7mu6u31BEwM",
-	authDomain: "rr-budget.firebaseapp.com",
-	databaseURL: "https://rr-budget.firebaseio.com",
-	projectId: "rr-budget",
-	storageBucket: "rr-budget.appspot.com",
-	messagingSenderId: "233069196525"
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.FIREBASE_URL,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 firebase.initializeApp(config);
 
