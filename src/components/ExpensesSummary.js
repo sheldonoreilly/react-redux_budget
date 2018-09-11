7240d4c3b496517c8ec0b716084667cbdd2e5e4e
Sheldon O'Reilly <sheldon.oreilly@gmail.com>
2018-09-10 21:53:08 -0230

    header and buttons.

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import selectExpenses from "../selectors/expenses";
 import selectExpensesTotal from "../selectors/expenses-total";
@@ -9,10 +10,18 @@ export const ExpensesSummary = props => {
 	const plural = props.expenseCount === 1 ? "expense" : "expenses";
 	const total = numeral(props.expensesTotal / 100).format("$0,0.00");
 	return (
-		<div>
-			<h1>
-				Viewing {props.expenseCount} {plural} totalling {total}
-			</h1>
+		<div className="page-header">
+			<div className="content-container">
+				<h1 className="page-header__title">
+					Viewing <span>{props.expenseCount}</span> {props.expenseword} {plural} totalling{" "}
+					<span>{total}</span>
+				</h1>
+				<div className="page-header__actions">
+					<Link className="button" to="/create">
+						Add Expense
+					</Link>
+				</div>
+			</div>
 		</div>
 	);
 };
